429f398b8f1d6989a3be8654bd490b613bbc72ed
donuts-are-good <96031819+donuts-are-good@users.noreply.github.com>
2023-04-07 04:00:37 -0500

    fixes #1

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,6 +14,17 @@ import (
 )
 
 func main() {
+	var destination string
+	flag.StringVar(&destination, "destination", "./repos", "The destination directory for cloned repositories")
+	flag.Parse()
+
+	if _, err := os.Stat(destination); os.IsNotExist(err) {
+		if err := os.Mkdir(destination, 0755); err != nil {
+			fmt.Printf("Error creating destination directory: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
 	token := os.Getenv("GITHUB_TOKEN")
 	if token == "" {
 		fmt.Println("GITHUB_TOKEN environment variable not set.")
@@ -24,29 +37,66 @@ func main() {
 	client := github.NewClient(tc)
 
 	// list all repositories, public in this case
-	opt := &github.RepositoryListOptions{Type: "public"}
-	repos, _, err := client.Repositories.List(context.Background(), "", opt)
+	opt := &github.RepositoryListOptions{Type: "public", ListOptions: github.ListOptions{PerPage: 100}}
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.Repositories.List(context.Background(), "", opt)
+		if err != nil {
+			fmt.Printf("💀 error listing repositories: %v\n", err)
+			os.Exit(1)
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	user, _, err := client.Users.Get(context.Background(), "")
 	if err != nil {
-		fmt.Printf("💀 error listing repositories: %v\n", err)
+		fmt.Printf("💀 error getting user info: %v\n", err)
 		os.Exit(1)
 	}
 
-	// for each repo we find, clone it
-	for _, repo := range repos {
+	fmt.Printf("Cloning all repos for user: github.com/%s\n", *user.Login)
+	fmt.Printf("Found: %d repos\n", len(allRepos))
+
+	// for each repo we find, clone or pull it
+	for i, repo := range allRepos {
 		cloneURL := *repo.CloneURL
 		repoName := *repo.Name
-		destPath := filepath.Join(".", repoName)
+		destPath := filepath.Join(destination, repoName)
 
-		// announce each repo as we clone it
-		fmt.Printf("👷 cloning %s...\n", cloneURL)
+		if _, err := os.Stat(destPath); os.IsNotExist(err) {
+			// announce each repo as we clone it
+			fmt.Printf("[ %0*d / %d ] ✅ 📦 cloned: %s ", len(fmt.Sprint(len(allRepos))), i+1, len(allRepos), repoName)
 
-		cmd := exec.Command("git", "clone", cloneURL, destPath)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		err := cmd.Run()
-		if err != nil {
-			fmt.Printf("💀 error cloning repository %s: %v\n", repoName, err)
-			continue
+			cmd := exec.Command("git", "clone", cloneURL, destPath)
+			var output bytes.Buffer
+			cmd.Stdout = &output
+			cmd.Stderr = &output
+			err := cmd.Run()
+			if err != nil {
+				fmt.Printf("❌\n")
+				fmt.Printf("Error cloning repository %s: %v\n", repoName, output.String())
+				continue
+			}
+			fmt.Printf("\n")
+		} else {
+			// announce each repo as we pull it
+			fmt.Printf("[ %0*d / %d ] ✅ ⬇️  pulled: %s ", len(fmt.Sprint(len(allRepos))), i+1, len(allRepos), repoName)
+
+			cmd := exec.Command("git", "-C", destPath, "pull", "--rebase")
+			var output bytes.Buffer
+			cmd.Stdout = &output
+			cmd.Stderr = &output
+			err := cmd.Run()
+			if err != nil {
+				fmt.Printf("❌\n")
+				fmt.Printf("Error pulling repository %s: %v\n", repoName, output.String())
+				continue
+			}
+			fmt.Printf("\n")
 		}
 	}
 }
